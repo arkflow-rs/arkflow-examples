585cb947718ac4bf746a67fa64e14b21b80962e0
chenquan <chenquan.dev@gmail.com>
2025-04-19 10:30:06 +0800

    x

--- input/src/bin/arkflow_input.rs
+++ input/src/bin/arkflow_input.rs
@@ -1,6 +1,7 @@
 use arkflow_core::cli::Cli;
 use arkflow_plugin::{buffer, input, output, processor};
 use crate::input as input_example;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     input::init();
