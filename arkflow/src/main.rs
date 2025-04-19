f3b588f9c7c112c197a7abaa52e1705c1e1923b5
chenquan <chenquan.dev@gmail.com>
2025-04-19 10:39:53 +0800

    x

--- arkflow/src/main.rs
+++ arkflow/src/main.rs
@@ -1,7 +1,7 @@
 use arkflow_core::cli::Cli;
 use input as input_example;
 use arkflow_plugin::{buffer, input as stable_input, output, processor};
-
+use sql_udf as sql_udf_example;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     stable_input::init();
@@ -10,7 +10,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     buffer::init();
 
     input_example::init();
-    sql_udf::init();
+    sql_udf_example::init();
     let mut cli = Cli::default();
     cli.parse()?;
     cli.run().await
