3c8498deb848acb65bb27ea5bddb5e8d670eabbe
chen quan <chenquan.dev@gmail.com>
2025-04-19 10:48:31 +0800

    feat: add input (#1)
    * feat: add input
    
    * x
    
    * x
    
    * x
    
    * x
    
    * x
    
    * x

--- arkflow/src/main.rs
+++ arkflow/src/main.rs
@@ -1,13 +1,16 @@
 use arkflow_core::cli::Cli;
-use arkflow_plugin::{buffer, input, output, processor};
-
+use input as input_example;
+use arkflow_plugin::{buffer, input as stable_input, output, processor};
+use sql_udf as sql_udf_example;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    input::init();
+    stable_input::init();
     output::init();
     processor::init();
     buffer::init();
-    sql_udf::init();
+
+    input_example::init();
+    sql_udf_example::init();
     let mut cli = Cli::default();
     cli.parse()?;
     cli.run().await
---
+++ input/src/lib.rs
@@ -0,0 +1,41 @@
+use std::sync::Arc;
+use arkflow_core::{Error, MessageBatch};
+use arkflow_core::input::{Ack, Input, InputBuilder, NoopAck};
+use async_trait::async_trait;
+use tracing::info;
+
+struct InputExample;
+
+#[async_trait]
+impl Input for InputExample {
+    async fn connect(&self) -> Result<(), Error> {
+        // do nothing
+        info!("InputExample::connect");
+
+        Ok(())
+    }
+
+    async fn read(&self) -> Result<(MessageBatch, Arc<dyn Ack>), Error> {
+        // Read the data
+        tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+        info!("InputExample::read");
+        Ok((MessageBatch::new_binary(vec!["{}".to_string().into_bytes()])?, Arc::new(NoopAck)))
+    }
+
+    async fn close(&self) -> Result<(), Error> {
+        // do nothing
+        info!("InputExample::close");
+        Ok(())
+    }
+}
+
+struct InputExampleBuilder;
+impl InputBuilder for InputExampleBuilder {
+    fn build(&self, _: &Option<serde_json::value::Value>) -> Result<Arc<dyn Input>, Error> {
+        Ok(Arc::new(InputExample))
+    }
+}
+
+pub fn init() {
+    arkflow_core::input::register_input_builder("input_example", Arc::new(InputExampleBuilder));
+}
