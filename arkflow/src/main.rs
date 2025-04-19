a48fb298c13236874c0a437cc7cb98bef427f44a
chenquan <chenquan.dev@gmail.com>
2025-04-19 10:38:59 +0800

    x

--- arkflow/src/main.rs
+++ arkflow/src/main.rs
@@ -1,14 +1,16 @@
 use arkflow_core::cli::Cli;
-use arkflow_plugin::{buffer, input, output, processor};
-use crate::input as input_example;
+use input as input_example;
+use arkflow_plugin::{buffer, input as stable_input, output, processor};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    input::init();
+    stable_input::init();
     output::init();
     processor::init();
     buffer::init();
+
     input_example::init();
+    sql_udf::init();
     let mut cli = Cli::default();
     cli.parse()?;
     cli.run().await
